d33fe00726636b18e7bcb1f0c4fe2b2310d2c7cb
ericpedley@gmail.com <ericpedley@gmail.com>
2020-06-21 13:52:51 -0700

    removed request library

--- app.js
+++ app.js
@@ -8,7 +8,6 @@
  */
 
 var express = require('express'); // Express web server framework
-var request = require('request'); // "Request" library
 var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
